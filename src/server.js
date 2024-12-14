ef9971559968608d1a25b59903e5d2fa9dbcfd91
JoaquinIppolito <joaquinippolito88@gmail.com>
2024-12-14 17:04:41 -0300

    correcion imagenes base64

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use(customCorsMiddleware)
 //Middleware que habilita las consultas de origen cruzado
 app.use(cors())
 
-app.use(express.json())
+app.use(express.json({ limit: '2mb' }));
 
 app.use('/api/status', statusRouter)
 app.use('/api/auth', authRouter)
